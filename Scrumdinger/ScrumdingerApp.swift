57ced04d14c4c3b1d017de66186a9e61b336f989
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-01-25 11:56:31 +0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Muhammad Asad Chattha on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
