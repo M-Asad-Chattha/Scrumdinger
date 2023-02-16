01a46c7f33ff5ce6d549db05eb3c72d0cf791561
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-02-16 14:19:04 +0500

    Complete "Persisting Data" in "Persistence-and-Concurrency"

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
-    @State private var scrums = DailyScrum.sampleData
+    //@State private var scrums = DailyScrum.sampleData
+    @StateObject private var store = ScrumStore()
     
     var body: some Scene {
         WindowGroup {
             NavigationStack {
-                ScrumsView(scrums: $scrums)
+                ScrumsView(scrums: $store.scrums) {
+                    ScrumStore.save(scrums: store.scrums) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
                 // Test()
+            }
+            .onAppear {
+                ScrumStore.load { result in
+                    switch result {
+                    case .success(let scrums):
+                        store.scrums = scrums
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    }
+                }
             }
         }
     }
